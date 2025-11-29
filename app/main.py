320273d7f3f3d0fdd4beecc105bd5125e6481c8d
Jasmim Mattos <101755858+jfmattos@users.noreply.github.com>
2025-11-29 16:21:01 -0300

    creating routers

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
+from app.routers import health
 
 # creating the app FastAPI
 app = FastAPI()
+
+# linking health router 
+app.include_router(health.router)
+
 
 # endpoint to test the root
 @app.get("/")
 def status():
     return {"yeah": "it's working"}
-
-# endpoint to test the api
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
